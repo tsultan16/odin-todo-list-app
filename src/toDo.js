0e3c94cb4f97f87060a636974c1ed004e6d8caab
tsultan16 <tanzids@student.unimelb.edu.au>
2025-01-24 18:45:21 +1100

    created a random date generator and function for creating sample items

--- src/toDo.js
+++ src/toDo.js
@@ -2,13 +2,19 @@
 export class toDo {
     static id = 0;
 
-    constructor (title, description, dueDate, priority=null) {
+    constructor (title, description, dueDate, started=false, priority=null) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
+        this.started = started;
         this.priority = priority;
+        this.completed = false;
         this.id = toDo.id
         toDo.id++;
+    }
+
+    complete () {
+        this.completed = true;
     }
 
 
@@ -17,7 +23,8 @@ export class toDo {
 // this class only responsible for storing/modifying a list of todo items
 export class Project {
 
-    constructor () {
+    constructor (title="Default") {
+        this.title = title;
         this.toDoItems = {};
     }
 
@@ -33,7 +40,33 @@ export class Project {
         return this.toDoItems[item_id];
     }
 
+    // need a way to track percentage completed, i.e. fraction of items completed
+
 }
+
+
+
+
+
+// sample task creation
+let sample_task_id = 0;
+
+export function createSampleTask() {
+    const item = new toDo(`title${sample_task_id}`, `desc${sample_task_id}`, randomDate());
+    sample_task_id++;
+
+    return item;
+}
+
+// create a random date between [from, to] range
+function randomDate (from = new Date(2025, 0, 21), to = new Date(2025, 11, 31)) {
+    return new Date(from.getTime() + Math.random() * (to.getTime() - from.getTime()));
+}
+
+
+
+
+
 
 
 
